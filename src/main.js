b36824479655dcfbe4bde8cd30111c1ba0d55811
newway-anshul <itsaj123@gmail.com>
2020-07-26 13:37:24 +0530

    Added ball movement,collision detection with platform,platform movement

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@
  */
 let bo_canvas = null;
 let ctx = null;
+let timer = null;
 let bo_default_config = {
   height: 300,
   width: 600,
@@ -19,6 +20,7 @@ let bo_default_config = {
   ballColor: "#38aad0f2",
   textFont: "20px serif",
   textColor: "#ffffff",
+  ballSpeed: 1,
 };
 const bo_level = [10, 10, 10];
 let tile_x_gap = 8;
@@ -31,15 +33,21 @@ let score_x = 10;
 let score_y = 20;
 let level_x = 500;
 let level_y = 20;
-
 let tile_x = ini_tile_x;
 let tile_y = ini_tile_y;
 let platForm_config = {
   x: 260,
   y: 265,
-  w: 60,
-  h: 15,
+  w: 90,
+  h: 10,
 };
+let ball_x = 300;
+let ball_y = 200;
+let ball_x_increment = 1;
+let ball_y_increment = 1;
+let ball_x_direction = 1;
+let ball_y_direction = 1;
+const ball_radius = 10;
 function set_game(congig_obj = bo_default_config) {
   bo_default_config = congig_obj;
   bo_canvas = document.getElementById("bo-main-scene");
@@ -48,12 +56,26 @@ function set_game(congig_obj = bo_default_config) {
   bo_canvas.width = congig_obj.width;
   bo_canvas.style.backgroundColor = congig_obj.backGroundColor;
   AddTile(...Object.values(platForm_config));
-  AddBall(300,200,10);
+  AddBall(ball_x, ball_y);
   renderText("Score:", score_x, score_y);
   renderText("Level:", level_x, level_y);
   renderLevel();
+  movePlateForm();
+  moveBall();
 }
-
+function movePlateForm() {
+  document.addEventListener("mousemove", function (e) {
+    const mouse_x = e.clientX;
+    if (
+      mouse_x >= ini_tile_x &&
+      mouse_x <= bo_default_config.width - ini_tile_x - platForm_config.w
+    ) {
+      canvasClear(...Object.values(platForm_config));
+      platForm_config.x = mouse_x;
+      AddTile(...Object.values(platForm_config));
+    }
+  });
+}
 /**
  *
  * @description {*} AddTile
@@ -65,17 +87,76 @@ function AddTile(x, y, w = tile_width, h = tile_height) {
   ctx.fillStyle = bo_default_config.tileColor;
   canvasFillRect(x, y, w, h);
 }
-function AddBall(x,y,r){
-  ctx.fillStyle  = bo_default_config.ballColor;
-  canvasArc(x,y,r)
+function AddBall(x, y, r = ball_radius) {
+  ctx.fillStyle = bo_default_config.ballColor;
+  canvasArc(x, y, r);
 }
 function canvasFillRect(x, y, w = tile_width, h = tile_height) {
-  ctx.fillRect(x, y, w, h);
-}
-function canvasArc(x,y,r){
   ctx.beginPath();
-  ctx.arc(x,y,r,0,2*Math.PI);
+  ctx.fillRect(x, y, w, h);
   ctx.fill();
+}
+function canvasArc(x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fill();
+}
+function canvasClear(x, y, w, h) {
+  ctx.clearRect(x, y, w, h);
+}
+function moveBall() {
+  ctx.fillStyle = bo_default_config.backgroundColor;
+  timer = setInterval(() => {
+    detectCollision();
+    canvasClear(
+      ball_x - ball_radius,
+      ball_y - ball_radius,
+      2 * ball_radius,
+      2 * ball_radius
+    );
+    switch (true) {
+      case ball_y >= bo_default_config.height - ball_radius ||
+        ball_y <= ball_radius:
+        ball_y_direction = -ball_y_direction;
+        break;
+      case ball_x >= bo_default_config.width - ball_radius ||
+        ball_x <= ball_radius:
+        ball_x_direction = -ball_x_direction;
+        break;
+      default:
+        break;
+    }
+    ball_x = ball_x + ball_x_direction * ball_x_increment;
+    ball_y = ball_y + ball_y_direction * ball_y_increment;
+    AddBall(ball_x, ball_y);
+  }, bo_default_config.ballSpeed);
+}
+function detectCollision() {
+  // detect platform collision
+  /**
+   * when ball hitting edges
+   */
+  if (ball_y == platForm_config.y - ball_radius) {
+    if (
+      ball_x >= platForm_config.x - ball_radius &&
+      ball_x <= platForm_config.x + platForm_config.w + ball_radius
+    ) {
+      ball_y_direction = -ball_y_direction;
+      return;
+    }
+  }
+  /*end*/
+  /*when ball is within the platform*/
+  if (ball_y >= platForm_config.y - ball_radius) {
+    if (
+      ball_x >= platForm_config.x - ball_radius &&
+      ball_x <= platForm_config.x + platForm_config.w + ball_radius
+    ) {
+      ball_y_direction = -ball_y_direction;
+      ball_x_direction = -ball_x_direction;
+    }
+  }
+  /*end*/
 }
 function renderLevel() {
   bo_level.forEach((element) => {
